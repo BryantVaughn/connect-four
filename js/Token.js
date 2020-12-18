e7024f856b2dd43d41ca363f3591b7d2da4238e3
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-18 13:23:04 -0700

    Add moveLeft and moveRight methods and apply them to Game conditionals

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,23 @@ class Token {
 		this.owner = owner;
 		this.id = `token-${index}-${owner.id}`;
 		this.dropped = false;
+		this.columnLocation = 0;
+	}
+
+	/**
+	 * Gets left offset of HTML element
+	 * @return {string} Left offset of token object's htmlToken
+	 */
+	get offsetLeft() {
+		return this.htmlToken.offsetLeft;
+	}
+
+	/**
+	 * Getter to return HTML Token element associated with Token object
+	 * @return {DOMElement} The DOM element associated with this Token object
+	 */
+	get htmlToken() {
+		return document.getElementById(`${this.id}`);
 	}
 
 	/**
@@ -17,10 +34,23 @@ class Token {
 	}
 
 	/**
-	 * Getter to return HTML Token element associated with Token object
-	 * @return {DOMElement} The DOM element associated with this Token object
+	 * Moves HTML token one column to left
 	 */
-	get htmlToken() {
-		return document.getElementById(`${this.id}`);
+	moveLeft() {
+		if (this.columnLocation !== 0) {
+			this.htmlToken.style.left = this.offsetLeft - 76;
+			this.columnLocation -= 1;
+		}
+	}
+
+	/**
+	 * Moves HTML token one column to right
+	 * @param {number} columns - number of columns in the game board
+	 */
+	moveRight(columns) {
+		if (this.columnLocation < columns - 1) {
+			this.htmlToken.style.left = this.offsetLeft + 76;
+			this.columnLocation += 1;
+		}
 	}
 }
--- js/app.js
+++ js/app.js
@@ -7,3 +7,7 @@ startBtn.addEventListener('click', function () {
 	this.style.display = 'none';
 	document.getElementById('play-area').style.opacity = '1';
 });
+
+document.addEventListener('keydown', function (evt) {
+	game.handleKeydown(evt);
+});
