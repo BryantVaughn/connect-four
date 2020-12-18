576349bb0e69e56f230b8938457edec29e92788e
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-18 12:20:03 -0700

    Add drawHTMLToken method and getter for Token

--- js/Token.js
+++ js/Token.js
@@ -4,4 +4,22 @@ class Token {
 		this.id = `token-${index}-${owner.id}`;
 		this.dropped = false;
 	}
+
+	/**
+	 * Creates a Token element and renders to board
+	 */
+	drawHTMLToken() {
+		const tokenDiv = document.createElement('div');
+		document.getElementById('game-board-underlay').appendChild(tokenDiv);
+		tokenDiv.setAttribute('id', this.id);
+		tokenDiv.setAttribute('class', 'token');
+		tokenDiv.style.backgroundColor = this.owner.color;
+	}
+
+	/**
+	 * Getter to return HTML Token element associated with Token object
+	 */
+	get htmlToken() {
+		return document.getElementById(`${this.id}`);
+	}
 }
