120566afc52833e3eb676648f811221f90a5b37c
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-12-18 17:14:59 -0700

    Cleanup js files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 const startBtn = document.getElementById('begin-game');
-const restartBtn = document.getElementById('restart');
 const instructions = document.querySelector('.instructions');
 
 const game = new Game();
@@ -9,11 +8,6 @@ startBtn.addEventListener('click', function () {
 	this.style.display = 'none';
 	instructions.style.display = 'block';
 	document.getElementById('play-area').style.opacity = '1';
-});
-
-restartBtn.addEventListener('click', function () {
-	game.startGame();
-	this.parentNode.style.display = 'none';
 });
 
 document.addEventListener('keydown', function (evt) {
